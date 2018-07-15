9f3b0c1884d8abdd1c31bdba382954e292ebe50c
Guilherme <gui_sc@outlook.com>
2018-07-14 23:01:33 -0300

    Fixed minor bugs in menus and fineshed Menu implementation.

--- SimpleRPG.ConsoleAPP/Menus/MainMenu.cs
+++ SimpleRPG.ConsoleAPP/Menus/MainMenu.cs
@@ -6,29 +6,41 @@ namespace SimpleRPG.ConsoleAPP.Menus
 {
     class MainMenu : Menu
     {
-        public String Option { get; set; } = "";
+        public String Option { get; set; } = "just started";
 
-        public void IniMenu()
+        public void InitMenu()
         {
             do
             {
+                //Stop the comand line to see the result of what the person choosed!
+                if (!Option.Equals("just started")) Console.ReadKey();
+
+                //Show menu and the let the user write what he wants
+                ShowMenu();
+                ChoseOption();
+
+                //Based on the user option program will fallow the right flow.
                 switch (Option)
                 {
                     case "1":
                         Console.WriteLine("Yet to implement!");
                         break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
                 }
             } while (!Option.Equals("quit"));
         }
 
-        public void ShowMenu()
+        private void ShowMenu()
         {
-            Console.WriteLine("Hello, this is SimpleRPG(quit to exit)\n\n");
+            Console.Clear();
+            Console.WriteLine("Hello, this is SimpleRPG(quit to exit)\n");
             Console.WriteLine("1 - Create character");
             Console.Write(">");
         }
 
-        public void ChoseOption()
+        private void ChoseOption()
         {
             this.Option = Console.ReadLine();
         }
